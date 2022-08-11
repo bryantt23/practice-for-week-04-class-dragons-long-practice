b3f72ea9da46799aa4586835a5675a53bc11e2dc
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-10 21:30:24 -0700

    pass tests

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,21 @@
 // Your code here
+class Dragon {
+  static dragons = [];
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+    if (!Dragon.dragons.some(name => name === this.name)) {
+      Dragon.dragons.push(this.name);
+    }
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+  static getDragons() {
+    return Dragon.dragons;
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
