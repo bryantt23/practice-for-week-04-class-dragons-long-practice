3b323065dc59baaeb0b1a38aea343a88ec17b811
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-10 21:38:46 -0700

    pass tests

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,28 @@
 // Your code here
+
+const Dragon = require('./dragon');
+// Your code here// Your code here
+class EvilDragon extends Dragon {
+  static dragons = [];
+  constructor(name, color, evilDoings = [], nemesis) {
+    super(name, color);
+    this.evilDoings = evilDoings;
+    this.nemesis = nemesis;
+  }
+  dontInviteThemOverForDinner() {
+    for (const evilDoing of this.evilDoings) {
+      console.log(`${this.name} will ${evilDoing}`);
+    }
+  }
+  burnsNemesis() {
+    return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+  }
+}
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = EvilDragon;
+} catch {
+  module.exports = null;
+}
