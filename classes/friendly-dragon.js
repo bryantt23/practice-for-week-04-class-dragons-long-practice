2b0abe1db2acbe7e061a4498669a60443a48a218
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-08-10 21:36:03 -0700

    pass tests

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,26 @@
-// Your code here
+const Dragon = require('./dragon');
+// Your code here// Your code here
+class FriendlyDragon extends Dragon {
+  static dragons = [];
+  constructor(name, color, lifeGoals = [], friend) {
+    super(name, color);
+    this.lifeGoals = lifeGoals;
+    this.friend = friend;
+  }
+  hasLifeGoals() {
+    for (const lifeGoal of this.lifeGoals) {
+      console.log(`${this.name} likes to ${lifeGoal}`);
+    }
+  }
+  helpsPeople() {
+    return `${this.name} helps their friend ${this.friend}`;
+  }
+}
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = FriendlyDragon;
+} catch {
+  module.exports = null;
+}
